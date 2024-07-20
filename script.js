9e61eeece45736efd99d1bde3e41020b4224a90b
Burakkuboido <Burakkuboido@hotmail.com>
2024-07-20 15:37:27 +0100

    Local storage implemented. Functions work as intended.

--- script.js
+++ script.js
@@ -14,15 +14,15 @@ document.querySelectorAll('.tab').forEach(item => {
         }
     })
   })
-
-
+showTask();
+grabListElements();
 
 
 listContainer = document.getElementById('listContainer');
 
 function addCat() {
     let cat = prompt("Add Category");
-if (cat == "") {
+if (cat == "" || cat == null) {
     alert("Did you forget to type something in? Or are you just wasting my time?")
 } else {
     let header = document.createElement("h5");
@@ -33,25 +33,30 @@ if (cat == "") {
     span.innerHTML = "\u00d7"
     header.appendChild(span);
     span.classList.add("hide");
+    grabListElements();
 }
-list = document.querySelectorAll('.list');
-document.querySelectorAll('.list').forEach(item => {
-    item.addEventListener('mouseover', (event) => {
-        event.target.querySelector('span').classList.remove('hide')
+saveData();
+}
+
+function grabListElements() {
+    list = document.querySelectorAll('.list');
+    document.querySelectorAll('.list').forEach(item => {
+        item.addEventListener('mouseover', (event) => {
+            event.target.querySelector('span').classList.remove('hide')
+        })
     })
-})
-document.querySelectorAll('.list').forEach(item => {
-    item.addEventListener('mouseleave', (event) => {
-        event.target.querySelector('span').classList.add('hide')
-    })
-})
+    document.querySelectorAll('.list').forEach(item => {
+        item.addEventListener('mouseleave', (event) => {
+            event.target.querySelector('span').classList.add('hide')
+        })
+    })    
 }
 
 
 
 function addTask() {
     let task = prompt("Add Task");
-    if (task == "") {
+    if (task == "" || task == null) {
         alert("Did you forget to type something in? Or are you just wasting my time?")
     } else {
         let li = document.createElement("li");
@@ -61,6 +66,7 @@ function addTask() {
         span.innerHTML = "\u00d7"
         li.appendChild(span);
     }
+    saveData();
 }
 
 
@@ -71,4 +77,81 @@ listContainer.addEventListener("click", function(e) {
     } else if(e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
     }
+    saveData();
 }, false);
+
+
+function getListElements(){
+    var h5 = listContainer.getElementsByTagName("h5");
+    for (let x = 0; x < h5.length; x++) {
+        h5[x].onmousedown = pickItem;
+        h5[x].onmouseover = moveItem;
+    }
+    var li = listContainer.getElementsByTagName("li");
+    for (let x = 0; x < li.length; x++) {
+        li[x].onmousedown = pickItem;
+        li[x].onmouseover = moveItem;
+}
+    saveData();
+}
+
+
+setInterval(getListElements, 1000);
+//// Get the list items inside of the dragable list
+
+//// Item to move
+var pick;
+//// Where to drop item
+var target;
+//// Save CSS style of picked up item
+var lastStyle;
+// END VARIABLES //
+
+// Add an event listener to each item in the draggableList
+
+
+// Moves the item
+function moveItem () {
+  target = this
+  target.location = event.clientY;
+  if(pick)
+    if(pick.location > target.location) // Insert before if moving up
+      target.parentNode.insertBefore(pick, target)
+    else  // Insert after if moving down
+      target.parentNode.insertBefore(pick, target.nextSibling)
+}
+ 
+// Selects the item to move
+function pickItem () {
+  pick = this;
+  lastStyle = pick.style;
+  // Used to see if moving up or down the list
+  pick.location = event.clientY;  
+  
+  // Stop text from being selected when dragging
+  document.body.classList.add("noSelect");
+  this.style.backgroundColor = "rgb(37, 37, 37)";
+  
+  // Adds an event to the DOM when an item is picked up
+  document.onmouseup = dropItem;
+}
+
+// Places the item and clears the pick and event listeners
+function dropItem () {
+  pick.style = lastStyle;
+  pick = '';
+  document.body.classList.remove("noSelect");
+  document.removeEventListener("onmouseup", dropItem)
+}
+
+
+
+
+
+function saveData() {
+    localStorage.setItem("daily", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("daily");
+}
